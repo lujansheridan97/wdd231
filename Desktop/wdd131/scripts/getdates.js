16ad66607c45f378d7526d857b01c3122789aed4
lujansheridan97 <marialujansheridan@gmail.com>
2025-03-17 22:55:36 -0300

    week2

--- Desktop/wdd131/scripts/getdates.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Output the copyright year in the footer's first paragraph
-    let currentYear = new Date().getFullYear();
-    document.querySelector("footer p:first-of-type").textContent = `© ${currentYear} Your Company Name`;
-
-    // Output the last modified date in the second paragraph
-    let lastModifiedDate = document.lastModified;
-    document.querySelector("footer p:nth-of-type(2)").textContent = `Last modified: ${lastModifiedDate}`;
-});
